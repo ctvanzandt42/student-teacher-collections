5cd85d594e363e31a662e47707550c8ceeecf040
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-26 08:10:31 -0500

    almost there..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Curtis", "Vanzandt", 2103, 1));
+        students.add(new Student("John", "Jastrow", 2104, 1));
+        students.add(new Student("Paul", "Dantam", 2105, 1));
+        students.add(new Student("Evan", "Coakley", 2106, 1));
+        students.add(new Student("Tyler", "Jurecki", 2107, 1));
+        students.add(new Student("Hamilton", "Moore", 2108, 1));
+        students.add(new Student("Jill", "Somethingkindapolish", 2109, 1));
+        students.add(new Student("Erin", "Woody", 2110, 1));
+        students.add(new Student("Buffy", "Vanzandt", 2111, 1));
+        students.add(new Student("Dakota", "Nichols", 2112, 1));
+        students.add(new Student("Andrew", "Gibson", 2113, 1));
+        students.add(new Student("Tanner", "Plauche", 2114, 1));
+        students.add(new Student("Bethany", "Wynns", 2115, 1));
+        students.add(new Student("Hannah", "Vanzandt", 2116, 1));
+        students.add(new Student("Pennywise", "Dancing-Clown", 2117, 1));
+
+        List<Teacher> teachers = new ArrayList<>();
+
+        teachers.add(new Teacher("James", "Harden", 2118, 1));
+        teachers.add(new Teacher("LeBron", "James", 2119, 1));
+        teachers.add(new Teacher("Kevin", "Durant", 2120, 1));
+
+        HashSet<String> students1 = new HashSet<>();
+        students1.add(students.get());
+
+
+
+
+
+
 
     }
 }
